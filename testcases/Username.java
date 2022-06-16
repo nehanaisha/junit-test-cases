0c34eae94c967bff7e9e9901ec995d7055fb507e
nehanaisha <nehachaurasia682@gmail.com>
2022-06-17 00:04:47 +0530

    passwordvalidation

--- testcases/Username.java
+++ testcases/Username.java
@@ -4,7 +4,11 @@ import java.util.regex.Pattern;
 
 public class Username {
 	public String validUsername(String username) {
-		System.out.println(Pattern.matches("[A-Za-z]{6}", username));
+		System.out.println(Pattern.matches("[A-Z]+[A-Za-z]{6}", username));
 		return username;
+	}
+	public String validPassword(String password) {
+		System.out.println(Pattern.matches("^([A-Z]?)"+"([a-z]?)"+"([0-9]?)"+"([@!$]?.{8,20})", password));
+		return password;
 	}
 }
--- testcases/UsernameTest.java
+++ testcases/UsernameTest.java
@@ -3,13 +3,18 @@ package testcases;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
 class UsernameTest {
-	
 
 	Username test = new Username();
+
 	@Test
 	public void testname() {
-		assertEquals(true, test.validUsername("Nehacc"));
+		assertEquals(true, test.validUsername("Nehammc"));
 	}
 
+	@Test
+	public void testpassword() {
+		assertEquals(true, test.validPassword("Neha@201"));
+	}
 }
